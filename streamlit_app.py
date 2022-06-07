c1835f9bfa9e50c0f0b32817a33edeffe7b1e56f
AgeBakker <AgeBakker@users.noreply.github.com>
2022-06-07 19:19:29 +0200

    Update streamlit_app.py
    Added smoothie item

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥬Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚Hard-Bolied Free-Range Egg')
 streamlit.text('🥑Avocado Toast')
+
+streamlit.header('🍌🍓Build Your Own Fruit Smoothie🥝🍇')
