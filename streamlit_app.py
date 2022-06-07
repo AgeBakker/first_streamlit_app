8fda384b2db1dfd8bf375fb2db3a2ab6fb9277c1
AgeBakker <AgeBakker@users.noreply.github.com>
2022-06-07 19:23:22 +0200

    Update streamlit_app.py
    Added my fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,6 @@ streamlit.text('🥚Hard-Bolied Free-Range Egg')
 streamlit.text('🥑Avocado Toast')
 
 streamlit.header('🍌🍓Build Your Own Fruit Smoothie🥝🍇')
+
+my_fruit_list=pandas.read("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
