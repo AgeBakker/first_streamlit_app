4c1ec1135eda2df537b4c81732f91289cd9df19b
AgeBakker <AgeBakker@users.noreply.github.com>
2022-06-11 12:08:37 +0200

    Update streamlit_app.py
    Added dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,5 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-streamlit.text("The fruit load list contains: ")
-streamlit.text(my_data_row)
+streamlit.header("The fruit load list contains: ")
+streamlit.dataframe(my_data_row)
