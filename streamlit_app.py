93ab33efeb4cb13c84a9acd076bd9b026ce33d6e
AgeBakker <AgeBakker@users.noreply.github.com>
2022-06-11 12:09:35 +0200

    Update streamlit_app.py
    Added fetch all

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ streamlit.header('🍌🍓Build Your Own Fruit Smoothie🥝🍇')
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
@@ -23,4 +23,4 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("The fruit load list contains: ")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
